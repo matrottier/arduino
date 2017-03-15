81f2c580595e1341acf634d793f031359155d1ba
matrottier <trottier.marcandre@gmail.com>
2017-03-15 13:07:32 -0400

    Add files via upload

---
+++ sketch_feb16a.ino
@@ -0,0 +1,61 @@
+//avec la batterie la température semble plus stable. 
+//les fils du servo sont a confirmé
+//vérifier la localisation du 3v et 5v sur la carte. Semble avoir 
+//un trou de trop a la fin a cote du 'IOREF'
+
+#include <Servo.h>
+
+//TMP36 Pin Variables
+int sensorPin = 1; //the analog pin the TMP36's Vout (sense) pin is connected to
+                        //the resolution is 10 mV / degree centigrade with a
+                        //500 mV offset to allow for negative temperatures
+uint8_t playButtonPin = 7;
+uint8_t servoPin = 9;
+uint8_t ledPin = 13;
+int pos = 0;
+Servo myServo;  
+
+/*
+ * setup() - this function runs once when you turn your Arduino on
+ * We initialize the serial connection with the computer
+ */
+void setup()
+{
+  Serial.begin(9600);  //Start the serial connection with the computer
+                       //to view the result open the serial monitor 
+}
+ 
+void loop()                     // run over and over again
+{
+ //getting the voltage reading from the temperature sensor
+ int reading = analogRead(sensorPin);  
+ 
+ // converting that reading to voltage, for 3.3v arduino use 3.3
+ float voltage = reading * 5.0;
+ voltage /= 1024.0; 
+ 
+ // print out the voltage
+ Serial.print(voltage); Serial.println(" volts");
+ 
+ // now print out the temperature
+ float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheit
+ float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+ Serial.print(temperatureF); Serial.println(" degrees F");
+
+ myServo.attach(servoPin);
+ for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+ 
+ delay(1000);                                     //waiting a second
+}
